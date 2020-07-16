e56e1cb47515e1be724322b82aafd01b7fb72692
Sameer Ahmed <sameerahmed315505@gmail.com>
2020-07-17 04:10:30 +0500

    keras to tf

--- detect_mask_video.py
+++ detect_mask_video.py
@@ -2,9 +2,9 @@
 # python detect_mask_video.py
 
 # import the necessary packages
-from keras.applications.mobilenet_v2 import preprocess_input
-from keras.preprocessing.image import img_to_array
-from keras.models import load_model
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
 from imutils.video import VideoStream
 import numpy as np
 import argparse
