f1b21fbf31ae4c6eb87035248d9e35d174510747
Sameer Ahmed <sameerahmed315505@gmail.com>
2020-07-23 19:06:22 +0500

    ultrasonic sensor added

--- MLX90614_IR/MLX90614_IR.ino
+++ MLX90614_IR/MLX90614_IR.ino
@@ -17,33 +17,75 @@ char *typeName[]={"Object","Ambient"};
 
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
-void setup() {
-  Serial.begin(9600);
+// defines pins numbers
+const int trigPin = 9;
+const int echoPin = 10;
+// defines variables
+long duration;
+int distance;
 
-  Serial.println("Robojax MLX90614 test");  
+
+
+void setup() {
+ 
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+ 
+  Serial.begin(9600);
 
   mlx.begin();  
 }
 
 void loop() {
-  //Robojax Example for MLX90614
-  printTemp('C');
-  printTemp('D');
   
-  printTemp('F');  
-  printTemp('G'); 
+  //printTemp('C');
+  //printTemp('D');
+  
+  //printTemp('F');  
+  //printTemp('G');
+ 
+ // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance= duration*0.034/2;
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+ 
+ if (distance<20)
+ {
   if( getTemp('C')>40)
   {
-    //do something here
+    Serial.println("Temperature Excide");
+  }
+  else
+  {
+  Serial.println("Temperature Normal");
   }
   
-  printTemp('K');   
-  printTemp('L');  
-  Serial.println("======");
+  //printTemp('K');   
+  //printTemp('L');  
+  //Serial.println("======");
 
   delay(3000);
-  //Robojax Example for MLX90614
+  
+ }
+else{
+ Serial.println("Come Closer");
+ }
 }
+ 
+ 
+ 
+ 
+  
 
 /*
  * @brief returns temperature or relative humidity
